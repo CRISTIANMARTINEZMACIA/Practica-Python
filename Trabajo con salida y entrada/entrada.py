3fd7a877dfc7d625a513cadb2cc53c457dfc0b4a
CRISTIAN <CRISTIAN@LAPTOP-O4DAP881>
2023-03-04 22:10:11 -0500

    Primeros programas en python

---
+++ Trabajo con salida y entrada/entrada.py
@@ -0,0 +1,8 @@
+print("WELCOME TO THE GREETER PROGRAM")
+name = input("Enter your name: ")
+print("Grettings "+ name)
+
+print("Calculator program")
+firts_number = input("first number: ")
+second_number = input("second number: ")
+print(int(firts_number)+int(second_number))
